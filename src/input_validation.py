f3c54a04198f6d3e60ffa622f81f15977e9be975
niinaIsHere <niina.kampman@gmail.com>
2025-12-09 19:25:03 +0200

    input validation more methods

--- src/input_validation.py
+++ src/input_validation.py
@@ -1,4 +1,10 @@
 
+def validate_type(type):
+    pass
+
+def validate_title(title):
+    if not (2 <= len(title) <= 250):
+        return "Title length must be in range 2-250"
 
 def validate_year(year):
     if not year.isdigit():
@@ -8,10 +14,32 @@ def validate_year(year):
     if int(year) < 1900 or int(year) > 2099:
         return "Year out of range"
 
-def validate_title(title):
+def validate_author(author):
+    if not (2 <= len(author) <= 150):
+        return "Author length must be in range 2-150"
+    # check if only letters
+    if not author.isalpha():
+        return "Author must consist of only letters"
+    
+def validate_journal(journal):
     pass
 
-def validate_author(author):
+def validate_publisher(publisher):
     pass
+
+def validate_address(address):
+    pass
+
+
+
+# add more
+
+# contain into validate_text_input etc
+
+# goes into validate text: title
+# booktitle, journal, publisher, 
+
+#validate numbers: pages, 
+
 
 
